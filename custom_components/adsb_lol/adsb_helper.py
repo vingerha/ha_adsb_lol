a69666221bc6c60a94c16dd1e8ef6d57f705a837
Arjan <44190435+vingerha@users.noreply.github.com>
2024-03-16 09:32:39 +0100

    Fix selecting on altitude limit

--- custom_components/adsb_lol/adsb_helper.py
+++ custom_components/adsb_lol/adsb_helper.py
@@ -9,20 +9,19 @@ _LOGGER = logging.getLogger(__name__)
 def get_flight(self):
     _LOGGER.debug ("Get flight with data: %s", self)
     response = requests.get(self._url)
-    _LOGGER.debug ("Get flight: %s", response.json())
+    _LOGGER.debug ("Get flight rest output: %s", response.json())
     return response.json()
     
 def get_point_of_interest(self):
-    _LOGGER.debug ("Get poi with data: %s", self)
+    _LOGGER.debug ("Get flight poi with data: %s", self)
     response = requests.get(self._url)
-    _LOGGER.debug ("Get flight rest output: %s", response.json())
+    _LOGGER.debug ("Get flight poi rest output: %s", response.json())
     _response = []
     _response_h = []
     aircraft = {}
     aircraft_h = {}
     for ac in response.json()["ac"]:
-#        _LOGGER.debug ("Get ac in: %s", ac)
-        if self._altitude_limit == 0 or (self._altitude_limit > 0 and ac["alt_geom"] * 1000 / 0.3048 < self._altitude_limit):
+        if self._altitude_limit == 0 or (self._altitude_limit > 0 and ac["alt_geom"] < self._altitude_limit * 1000 / 0.3048 ):
             aircraft["callsign"] = ac.get("flight", None)
             aircraft["registration"] = ac.get("r", None)
             self._reg = ac.get("r", "NoReg")
@@ -36,13 +35,8 @@ def get_point_of_interest(self):
             aircraft["latitude"] = ac.get("lat", None)
             aircraft["longitude"] = ac.get("lon", None)
             aircraft_h[str(self._reg)] = aircraft.copy()
-#            _LOGGER.debug ("Get ac append _h: %s", aircraft_h)
-#            _LOGGER.debug ("Get ac append: %s", aircraft)
-           
-#        _response.append(aircraft.copy())
-#        _LOGGER.debug ("Get ac _response: %s", _response)
+
     _response_h = aircraft_h
-#    _LOGGER.debug ("Get flight transformed _h: %s", _response_h)
-#    _LOGGER.debug ("Get flight transformed: %s", _response)
-    
+
+    _LOGGER.debug ("Get flight poi: %s", response.json())
     return _response_h   
